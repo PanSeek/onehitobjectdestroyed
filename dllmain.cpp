3be9cb6a67db9deb5de7c357b315bedda2c4687f
PanSeek <35432222+PanSeek@users.noreply.github.com>
2021-09-09 22:07:40 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,25 +1,16 @@
 #include <Windows.h>
-#include "MinHook.h"
-
-typedef void(__cdecl* CTimer__Update) ();
-CTimer__Update oldCTimerHook = NULL;
-
-auto __cdecl hk_CTimer__Update(void) -> void {
-	DWORD oldProt;
-	VirtualProtect(reinterpret_cast<LPVOID>(0x005A0E1E), 8, PAGE_READWRITE, &oldProt);
-	memset(reinterpret_cast<void*>(0x005A0E1E), 0x90, 0x2);
-	VirtualProtect(reinterpret_cast<LPVOID>(0x005A0E1E), 8, oldProt, &oldProt);
-	return oldCTimerHook();
-}
 
 class CMain {
 public:
 	CMain() {
-		MH_Initialize();
-		MH_CreateHook(reinterpret_cast<void*>(0x561B10), &hk_CTimer__Update, reinterpret_cast<void**>(&oldCTimerHook));
-		MH_EnableHook(reinterpret_cast<void*>(0x561B10));
+		VirtualProtect(reinterpret_cast<LPVOID>(0x005A0E1E), 8, PAGE_READWRITE, &oldProt);
+		memset(reinterpret_cast<void*>(0x005A0E1E), 0x90, 0x2);
+		VirtualProtect(reinterpret_cast<LPVOID>(0x005A0E1E), 8, oldProt, &oldProt);
 	};
 	~CMain() {
-		MH_Uninitialize();
+		VirtualProtect(reinterpret_cast<LPVOID>(0x005A0E1E), 8, PAGE_READWRITE, &oldProt);
+		memcpy(reinterpret_cast<void*>(0x005A0E1E), "\x7A\x06", 0x2);
+		VirtualProtect(reinterpret_cast<LPVOID>(0x005A0E1E), 8, oldProt, &oldProt);
 	};
+	DWORD oldProt;
 }; CMain plugin;
