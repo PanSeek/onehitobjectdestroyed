b6fce0ea917878fe70962cfe6c58b4b4b5e9fbe2
PanSeek <35432222+PanSeek@users.noreply.github.com>
2022-01-30 20:56:48 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,16 +1,15 @@
-#include <Windows.h>
+#include <windows.h>
 
-class CMain {
-public:
-	CMain() {
-		VirtualProtect(reinterpret_cast<LPVOID>(0x005A0E1E), 2, PAGE_READWRITE, &oldProt);
-		memset(reinterpret_cast<void*>(0x005A0E1E), 0x90, 0x2);
-		VirtualProtect(reinterpret_cast<LPVOID>(0x005A0E1E), 2, oldProt, &oldProt);
+struct stMain {
+	stMain() {
+		VirtualProtect(reinterpret_cast<LPVOID>(0x5A0E1E), 0x2, PAGE_READWRITE, &oldProt);
+		memset(reinterpret_cast<void*>(0x5A0E1E), 0x90, 0x2);
+		VirtualProtect(reinterpret_cast<LPVOID>(0x5A0E1E), 0x2, oldProt, &oldProt);
 	};
-	~CMain() {
-		VirtualProtect(reinterpret_cast<LPVOID>(0x005A0E1E), 2, PAGE_READWRITE, &oldProt);
-		memcpy(reinterpret_cast<void*>(0x005A0E1E), "\x7A\x06", 0x2);
-		VirtualProtect(reinterpret_cast<LPVOID>(0x005A0E1E), 2, oldProt, &oldProt);
+	~stMain() {
+		VirtualProtect(reinterpret_cast<LPVOID>(0x5A0E1E), 0x2, PAGE_READWRITE, &oldProt);
+		memcpy(reinterpret_cast<void*>(0x5A0E1E), "\x7A\x06", 0x2);
+		VirtualProtect(reinterpret_cast<LPVOID>(0x5A0E1E), 0x2, oldProt, &oldProt);
 	};
 	DWORD oldProt;
-}; CMain plugin;
+} plugin;
